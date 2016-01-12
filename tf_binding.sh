e6e90bf2b30bd251e8136146fef472c3889650e3
tsheez <t2shaw@ucsd.edu>
2016-01-11 20:17:29 -0800

    Fixed some things in tf_binding.sh

--- tf_binding.sh
+++ tf_binding.sh
@@ -3,7 +3,7 @@
 #PBS -N tf_binding
 #PBS -l nodes=1:ppn=3
 #PBS -l walltime=0:10:00
-#PBS -o tf_bidning_out.o
+#PBS -o tf_binding_out.o
 #PBS -e tf_binding__error.e
 #PBS -V
 #PBS -M t2shaw@ucsd.edu
